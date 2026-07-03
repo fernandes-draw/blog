3e01bf8363322c32baeb9a060ba51b59188620a1
fernandes-draw <fernandes_draw@hotmail.com>
2026-07-03 16:24:55 -0300

    switching function-based view to generic class-based view

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import post_list, post_detail
 
+# from .views import post_list, post_detail
+from .views import BlogDetailView, BlogListView
+
+# FUNCTION-BASED VIEW
+# urlpatterns = [
+#     path("post/<int:pk>/", post_detail, name="post_detail"),
+#     path("", post_list, name="home"),
+# ]
+
+# GENERIC CLASS-BASED VIEW
 urlpatterns = [
-    path("post/<int:pk>/", post_detail, name="post_detail"),
-    path("", post_list, name="home"),
+    path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
+    path("", BlogListView.as_view(), name="home"),
 ]
