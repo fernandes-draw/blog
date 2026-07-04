deefc82622121ea10afe426153be758afc569dfb
fernandes-draw <fernandes_draw@hotmail.com>
2026-07-04 00:03:16 -0300

    Create a new BlogDeleteView, define path on url, create template post_delete and links in post_detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 
 # from .views import post_list, post_detail
-from .views import BlogCreateView, BlogDetailView, BlogListView, BlogUpdateView
+from .views import (
+    BlogCreateView,
+    BlogDeleteView,
+    BlogDetailView,
+    BlogListView,
+    BlogUpdateView,
+)
 
 # FUNCTION-BASED VIEW
 # urlpatterns = [
@@ -14,5 +20,6 @@ urlpatterns = [
     path("post/new/", BlogCreateView.as_view(), name="post_new"),
     path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
     path("post/<int:pk>/edit", BlogUpdateView.as_view(), name="post_edit"),
+    path("post/<int:pk>/delete/", BlogDeleteView.as_view(), name="post_delete"),
     path("", BlogListView.as_view(), name="home"),
 ]
